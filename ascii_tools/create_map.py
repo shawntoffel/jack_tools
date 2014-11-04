74fa5ad59facf69d5d8ec68e5ce60683f9a62498
Shawn Toffel <shawn.toffel@gmail.com>
2014-11-03 23:19:51 -0700

    Convert to per-row reading

--- ascii_tools/create_map.py
+++ ascii_tools/create_map.py
@@ -2,6 +2,7 @@
 
 import os
 import sys
+import re
 
 E_USAGE = ('Usage: %s <input>' % sys.argv[0])
 NUM_ARVS = 4
@@ -17,52 +18,34 @@ OUTPUT = os.path.normpath(sys.argv[3])
 
 class JackGenerator:
 	def __init__(self, num_points):
-		self.output = "let numPoints = " + `num_points` + ";\n"
+		self.output = "let numPoints = %d;\n" % num_points
 		self.output += "let data = Array.new(numPoints);\n"
-		self.point_count = 0;
+		self.count = 0;
 
+	def addRow(self, locations):
+		self.output += "let temp = Array.new(%d);\n" % (len(locations) + 1)
+		self.output += "let temp[0] = %d;\n" % (len(locations) + 1)
+		for index, l in enumerate(locations):
+			s = "let temp[%d] = %d;\n" % (index+1, l)
+			self.output += s
 
-	def addPoint(self, x, y):
-		self.output += "let temp = Array.new(2);\n"
-		self.output += "let temp[0] = " + `x` + ";\n"
-		self.output += "let temp[1] = " + `y` + ";\n"
-		self.output += "let data["+`self.point_count`+"] = temp;\n"
-#		self.output += "do temp.dispose();\n"
-
-		self.point_count += 1
+		self.output += "let data[%d] = temp;\n" % self.count
+		self.count += 1
 
 class MapReader:
 	def __init__(self, filename):
 		self.inputfile = open(filename)
-		self.x = 0;
-		self.y = 0;
-		self.y_count = 0;
-		self.points = []
+		self.lines = []
 
 		self.readMap()
 
 		self.inputfile.close()
 
 	def readMap(self):
-		while True:
-			c = self.inputfile.read(1)
-			if not c:
-				break
-			if c.isspace():
-				self.x += 8
-			if "\n" in c:
-				self.y_count += 1
-				if (self.y_count >= 16):
-					self.y = 0
-					self.y_count = 0
-				else:
-					self.y += 15
-				self.x = 0
-			if c == "|":
-				self.points.append([self.x, self.y])
-				self.x += 8
 
-
+		for line in self.inputfile:
+			locations = [m.start() for m in re.finditer('\|', line)]
+			self.lines.append(locations)
 
 def write_yield(data):
 	infile = open(TEMPLATE)	
@@ -84,9 +67,10 @@ def main():
 	try:
 		reader = MapReader(INPUT)
 
-		gen = JackGenerator(len(reader.points))
-		for point in reader.points:
-			gen.addPoint(point[0], point[1])
+		gen = JackGenerator(len(reader.lines))
+
+		for l in reader.lines:
+			gen.addRow(l)
 
 		write_yield(gen.output)	
 
